50433072f769f2d4a78f14ab5f7453bb722bd752
RAATSsss <RAATSsss@users.noreply.github.com>
2015-07-08 19:22:28 +0100

    create plot 1
    create plot 1 for the week 1 assignment

---
+++ plot1.R
@@ -0,0 +1,16 @@
+## this code read the household power consumption data and plot the histogram using data from 2007-02-01 to 2007-02-02
+## clear workspace and console, load package
+rm(list = ls())
+cat("\014")
+require(data.table)
+## read and subset data
+df <- fread("~/household_power_consumption.txt", sep=";", header = TRUE, colClasses = list(character=1:9))
+df$Date <- as.Date(df$Date,format = '%d/%m/%Y')
+ddf <- df[Date %between% c('2007-02-01', '2007-02-02')]
+## create plot
+plot1 <- hist(as.numeric(ddf$Global_active_power), col = 'red', xlab = 'Gloabal Active Power (kilowatts)',
+     main = 'Global Active Power')
+## export png file here
+png(filename = "~/plot1.png", width = 480, height = 480) 
+plot(plot1) 
+dev.off()
